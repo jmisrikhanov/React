34e679bece14c7cd703ee1ec36cff5752ad1f8e6
jmisrikhanov <jmisrikhanov@gmail.com>
2021-10-25 23:29:29 +0200

    tour project 1v

---
+++ 1.Tour-project/src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="big-title">
+      <h3>Popular Tour Places</h3>
+    </div>
+  );
+};
+
+export default Header;
