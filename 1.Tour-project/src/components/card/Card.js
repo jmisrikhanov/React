8855e588ae5202e1564ca67882dd9c97fe923129
jmisrikhanov <jmisrikhanov@gmail.com>
2021-10-26 10:28:40 +0200

    some functionality added to card.js

--- 1.Tour-project/src/components/card/Card.js
+++ 1.Tour-project/src/components/card/Card.js
@@ -1,37 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { data } from "../../helpers/tourData";
 import "./Card.css";
 
 const Card = () => {
+  const [showImage, setShowImage] = useState(true);
   return (
-    <div className="card-container">
-      {data.map((card) => {
-        return (
-          <div className="cards" key={card.id}>
-            <div className="title">
-              <h2>{card.title}</h2>
-            </div>
-            <img src={card.image} alt={card.title} />
-            <div className="card-over">
-              <p>{card.desc}</p>
-            </div>
-          </div>
-        );
-      })}
+    <div className="card-container" onClick={() => setShowImage(!showImage)}>
+      {showImage ? (
+        <>
+          {data.map((card) => {
+            return (
+              <div className="cards" key={card.id}>
+                <img src={card.image} alt={card.title} />
+              </div>
+            );
+          })}
+        </>
+      ) : (
+        <>
+          {data.map((card) => {
+            return (
+              <div className="cards" key={card.id}>
+                <div className="title">
+                  <h2>{card.title}</h2>
+                </div>
+                <img src={card.image} alt={card.title} />
+                <div className="card-over">
+                  <p>{card.desc}</p>
+                </div>
+              </div>
+            );
+          })}
+        </>
+      )}
     </div>
   );
 };
 
 export default Card;
-
-// <div className="card-container">
-//   <div className="cards">
-//     <div className="title">
-//       <h2>{data[0].title}</h2>
-//     </div>
-//   </div>
-//   <img src={data[0].image} alt="" />
-//   <div className="card-over">
-//     <p>{data[0].desc}</p>
-//   </div>
-// </div>;
