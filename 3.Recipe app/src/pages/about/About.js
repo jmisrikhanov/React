5e338eb93fcdea428993db8a28940da792a3479b
jmisrikhanov <jmisrikhanov@gmail.com>
2021-10-29 09:46:30 +0200

    AboutStyle added

--- 3.Recipe app/src/pages/about/About.js
+++ 3.Recipe app/src/pages/about/About.js
@@ -1,10 +1,31 @@
 import React from "react";
+import {
+  AboutContainer,
+  HeaderContainer,
+  InfoContainer,
+  StyledImage,
+} from "./AboutStyle";
+import codingSvg from "../../assets/coding.svg";
 
 const About = () => {
   return (
-    <div>
-      <h2>About</h2>
-    </div>
+    <AboutContainer>
+      <StyledImage src={codingSvg} alt="coding" />
+      <HeaderContainer>
+        <h1>Lorem ipsum dolor sit amet.</h1>
+      </HeaderContainer>
+      <InfoContainer>
+        <h1>Lorem, ipsum.</h1>
+        <h2>Lorem ipsum dolor sit amet.</h2>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui,
+          inventore commodi ea soluta eius repellat!
+        </h2>
+        <h2>
+          <a href="mailto:lorem@gmail.com">Send email</a> : loremipsum@gmail.com
+        </h2>
+      </InfoContainer>
+    </AboutContainer>
   );
 };
 
