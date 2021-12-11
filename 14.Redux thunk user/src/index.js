b48620dcfb1febeeb408267fe8fd63e1aef26a85
jmisrikhanov <jmisrikhanov@gmail.com>
2021-12-11 19:11:53 +0100

    updated index.js

--- 14.Redux thunk user/src/index.js
+++ 14.Redux thunk user/src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import userReducer from "./redux/reducers/userReducer";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+
+const store = createStore(userReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
