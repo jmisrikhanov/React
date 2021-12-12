0cff32e28757c72c2057b46e6a68a600766ad22b
jmisrikhanov <jmisrikhanov@gmail.com>
2021-12-12 12:42:18 +0100

    combined phone

--- 15.Redux shop/src/redux/reducers/index.js
+++ 15.Redux shop/src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import laptopReducer from "./laptopReducer";
+import phoneReducer from "./phoneReducer";
 
 const rootReducer = combineReducers({
   laptop: laptopReducer,
+  phone: phoneReducer,
 });
 
 export default rootReducer;
