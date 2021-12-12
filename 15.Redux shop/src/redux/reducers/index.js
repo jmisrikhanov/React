1e11d11a376da8d81e199b11be0348c13433939f
jmisrikhanov <jmisrikhanov@gmail.com>
2021-12-12 13:37:55 +0100

    combined watch

--- 15.Redux shop/src/redux/reducers/index.js
+++ 15.Redux shop/src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import laptopReducer from "./laptopReducer";
 import phoneReducer from "./phoneReducer";
+import watchReducer from "./watchReducer";
 
 const rootReducer = combineReducers({
   laptop: laptopReducer,
   phone: phoneReducer,
+  watch: watchReducer,
 });
 
 export default rootReducer;
