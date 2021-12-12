fdf0bcc1a16302480cb1393ed88e557defacd663
jmisrikhanov <jmisrikhanov@gmail.com>
2021-12-12 12:41:57 +0100

    setted up phone reducer

---
+++ 15.Redux shop/src/redux/reducers/phoneReducer.js
@@ -0,0 +1,17 @@
+import { SELL_PHONE } from "../types/types";
+
+const initialState = {
+  numberOfPhones: 100,
+};
+
+const phoneReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SELL_PHONE:
+      return { ...state, numberOfPhones: state.numberOfPhones - 1 };
+
+    default:
+      return state;
+  }
+};
+
+export default phoneReducer;
