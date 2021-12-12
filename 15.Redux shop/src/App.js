3f4d1477388e691811f4af2e3db2692d189a2411
jmisrikhanov <jmisrikhanov@gmail.com>
2021-12-12 13:25:57 +0100

    added phone component

--- 15.Redux shop/src/App.js
+++ 15.Redux shop/src/App.js
@@ -1,10 +1,12 @@
 import LaptopComp from "./components/LaptopComp";
+import PhoneComp from "./components/PhoneComp";
 
 function App() {
   return (
     <div className="App">
       <h1>Shop</h1>
       <LaptopComp />
+      <PhoneComp />
     </div>
   );
 }
